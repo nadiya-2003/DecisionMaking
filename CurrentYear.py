39f6691ad192fb3a7fdf3123a881c358d042fada
nadiya-2003 <154906455+nadiya-2003@users.noreply.github.com>
2024-09-24 10:46:30 +0530

    CurrentYear.py

--- CurrentYear.py
+++ CurrentYear.py
@@ -1,3 +1,18 @@
+'''
+Ask a user for their birth year encoded as two digits (like "62") and for the current year, also encoded as two digits (like "99"). Write a program to find the users current age in years.
+Input format:
+Input consists of 2 integers
+The first integer corresponds to the last 2 digits of the birth year
+The second integer corresponds to the last 2 digits of the current year
+Output format:
+Print the user's current age
+Refer below sample output for formatting.
+Sample Input:
+62
+00
+Sample Output:
+38
+'''
 
 birth_year_last_two = int(input("Enter the last two digits of your birth year: "))
 current_year_last_two = int(input("Enter the last two digits of the current year: "))
