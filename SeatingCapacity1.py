9e0ebddcbaf8017263756911717e6fb9238604c1
nadiya-2003 <154906455+nadiya-2003@users.noreply.github.com>
2024-09-27 13:50:29 +0530

    SeatingCapacity1.py

--- SeatingCapacity1.py
+++ SeatingCapacity1.py
@@ -1,19 +1,22 @@
 '''
-There are 3 labs in the CSE department(L1, L2, and L3) with a seating capacity of x, y, and z respectively. Find the lab which has minimal seating capacity.  
+There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z respectively. One of the 3 labs has been allocated for ACE training. Out of the available labs, find the lab which has minimal seating capacity.
 Input format:
-Input consists of 3 integers
+Input consists of 3 integers and a string
 The first input denotes the seating capacity of L1(a)
 The second input denotes the seating capacity of L2(b)
 The third input denotes the seating capacity of L3(c)
+The fourth input denotes the lab which is allocated for ACE training
 Output format:
-Print the minimal seating lab capacity
+Print the minimal seating capacity lab amongst the available labs.
 Refer the Sample output for formatting
 Sample Input:
 30
 40
 20
-Sample Output:
 L3
+Sample Output:
+L1
+
 '''
                        
 
