6f1e79553037279c5ad4cc4cdcaefa269f11d879
nadiya-2003 <154906455+nadiya-2003@users.noreply.github.com>
2024-09-27 13:49:37 +0530

    SeatingCapacity.py

--- SeatingCapacity.py
+++ SeatingCapacity.py
@@ -1,51 +1,20 @@
 '''
-Write a program to determine the fee amount to be collected from a student. 
-Refer the table below for fee details.
-  Student Type  
-  Student Type denoted as  
-  Fee Details  
-Merit Seat Day Scholar
-MSDS
-
-Tuition fee + Bus fee
-
-
-
-Merit Seat Hosteller
-MSH
-Tuition fee + Hostel fee
-
-Management Seat Day Scholar
-
-
-
-MGSDS
-
-150% of Tuition fee + Bus fee
-
-
-
-Management Seat Hosteller
-MGSH
-
-150% of Tuition fee + Hostel fee
-
-
-
-
+There are 3 labs in the CSE department(L1, L2, and L3) with a seating capacity of x, y, and z respectively. Find the lab which has minimal seating capacity.  
 Input format:
-The first input corresponds to the student type
-The second input corresponds to the tuition fee
-The third input corresponds to the bus fee or hostel fees
+Input consists of 3 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
 Output format:
-Print the total fee amount of the corresponding student with 2 decimal places.
-Refer below sample output for formatting
+Print the minimal seating lab capacity
+Refer the Sample output for formatting
 Sample Input:
-MSH
-40000
-50000
+30
+40
+20
 Sample Output:
-90000.00
+L3
+
 '''
 
 L1=int(input())
